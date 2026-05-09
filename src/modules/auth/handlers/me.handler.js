ec0cee5f75b79c274b54f6638f5c11bf1202cd0e
yaxyobekuz <pubgn9642@gmail.com>
2026-05-09 15:18:36 +0500

    feat: remove authentication module and related handlers

--- src/modules/auth/handlers/me.handler.js
+++
@@ -1,9 +0,0 @@
-import asyncHandler from "../../../middleware/asyncHandler.js";
-import * as service from "../services/auth.service.js";
-
-const me = asyncHandler(async (req, res) => {
-  const data = await service.me(req.user);
-  res.json({ success: true, data });
-});
-
-export default me;
--- src/modules/auth/validators/login.validator.js
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const loginSchema = z.object({
-  body: z.object({
-    login: z.string().min(2, "Login juda qisqa"),
-    password: z.string().min(6, "Parol kamida 6 belgi bo'lishi kerak"),
-  }),
-});
